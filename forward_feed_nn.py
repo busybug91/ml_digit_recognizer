0928ca8bff9a5c93271a148144e1fa5a21756992
Nitin Ahuja <nitinasu@gmail.com>
2018-02-22 09:19:37 -0800

    Add accuracy and use Relu activation function

--- forward_feed_nn.py
+++ forward_feed_nn.py
@@ -25,19 +25,23 @@ Layers
 import tensorflow as tf
 import numpy as np
 import csv
+import math
 from lib.file_reader import read_chunks
 
 
 # TODO: see if setting a seed is required at graph level
 # It would be needed if we run multiple sessions and want each session to have same random sequence
+tf.set_random_seed(0)
 
 input_file = open('data/train.csv',mode='r')
 input_reader = csv.reader(input_file, delimiter=',')
 next(input_reader) # skip header
 
 
+total_input_rows = 42000;
+mini_batch_size = 50;
 # instantiate generator
-input_batch=read_chunks(input_reader, 100)
+input_batch=read_chunks(input_reader, mini_batch_size)
 
 # Number of neurons in each layer
 L0 = 200
@@ -61,28 +65,28 @@ Y = tf.placeholder(tf.float32, [None, 10])
 # There are 200 neurons in layer 1. Each neuron requires 784 weights, one for each of the feature/ dimension of the image!
 W0 = tf.Variable(tf.truncated_normal([784, L0], stddev = stddev))
 
-# See if bias can also be initialized to a negative. If yes, then randomly initialize Bias as well.
-B0 = tf.Variable(tf.zeros(L0))
+# Bias should be positive. Instead of 0, Relu prefers a small positive number.
+B0 = tf.Variable(tf.ones(L0)/10) # 0.1
 
 # now we need weights of L1 neurons. Each neuron needs, L0 weights, one for each output of the previous layer
 W1 = tf.Variable(tf.truncated_normal([L0, L1], stddev = stddev))
-B1 = tf.Variable(tf.zeros(L1))
+B1 = tf.Variable(tf.ones(L1)/10)
 
 W2 = tf.Variable(tf.truncated_normal([L1, L2], stddev = stddev))
-B2 = tf.Variable(tf.zeros(L2))
+B2 = tf.Variable(tf.ones(L2)/10)
 
 W3 = tf.Variable(tf.truncated_normal([L2, L3], stddev = stddev))
-B3 = tf.Variable(tf.zeros(L3))
+B3 = tf.Variable(tf.ones(L3)/10)
 
 W4 = tf.Variable(tf.truncated_normal([L3, L4], stddev = stddev))
-B4 = tf.Variable(tf.zeros(L4))
+B4 = tf.Variable(tf.ones(L4)/10)
 
 
 # model setup
-Y0 = tf.nn.sigmoid((tf.matmul(X,W0) + B0))
-Y1 = tf.nn.sigmoid(tf.matmul(Y0,W1) + B1)
-Y2 = tf.nn.sigmoid(tf.matmul(Y1,W2) + B2)
-Y3 = tf.nn.sigmoid(tf.matmul(Y2,W3) + B3)
+Y0 = tf.nn.relu((tf.matmul(X,W0) + B0))
+Y1 = tf.nn.relu(tf.matmul(Y0,W1) + B1)
+Y2 = tf.nn.relu(tf.matmul(Y1,W2) + B2)
+Y3 = tf.nn.relu(tf.matmul(Y2,W3) + B3)
 probabilities = tf.matmul(Y3,W4) + B4 
 probabilities_scaled = tf.nn.softmax(probabilities)         # softmax is going to scale the probabilities for us!
 
@@ -92,6 +96,10 @@ probabilities_scaled = tf.nn.softmax(probabilities)         # softmax is going t
 
 # softmax_cross_entropy uses unscaled probabilities as it would do the scaling for us
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits = probabilities, labels = Y)
+
+# way to measure accuracy
+correct_predictions = tf.equal(tf.argmax(probabilities_scaled, 1), tf.argmax(Y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
 
 
 # an optimizer to minimize our distance/ cross entropy
@@ -103,12 +111,11 @@ init = tf.initialize_all_variables()
 session = tf.Session()
 session.run(init)
 
-for i in range(1,1000):
+for i in range(1, math.ceil(total_input_rows / mini_batch_size)):
     
     # we need a chunk of data to start processing
-    input_chunk = np.array(next(input_batch))
-
-    print('Iteration: {0}'.format(i))
+    input_chunk = np.array(next(input_batch))   
+    
     if input_chunk.size == 0:
         break
 
@@ -121,5 +128,9 @@ for i in range(1,1000):
 
     session.run(optimizer, feed_dict = train_data_dict)
 
+    if (i % 10 == 0):
+        print('Iteration: {0}'.format(i))
+        a = session.run([accuracy], feed_dict = train_data_dict)
+        print("Accuracy: " + str(a)) 
 
 input_file.close()
